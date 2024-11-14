29b2bfd78d07a7d769284a4073b89e39ea5221af
Christian Helbling <helbling@search.ch>
2024-11-14 22:24:43 +0100

    add api overview page

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Unofficial APIs, use at your own risk</h1>
+
+<h2>Demos</h2>
+
+<ul>
+	<li><a href="/api/causal-diagram-widget-demo.html">Causal Diagram Widget Demo</li>
+	<li><a href="/api/animation-widget-demo.html">Animation Widget Demo</a></li>
+</ul>
+
+
+<h2>JS modules</h2>
+<ul>
+	<li><a href="/api/causal-diagram-widget-standalone.mjs">Causal Diagram Widget</li>
+	<li><a href="/api/animation-widget-standalone.mjs">Animation Widget</a></li>
+</ul>
